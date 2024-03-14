f0c2834e9513b3234e88f789c898ccbb4175f29d
domparso <domparso@hotmail.com>
2024-03-14 17:55:55 +0800

    build scripts

---
+++ build.bat
@@ -0,0 +1,31 @@
+@echo off
+cls
+echo off
+echo.
+echo ----------------------------GoLand 编译----------------------------------
+echo. ----------------------------by ohxing-----------------------------------
+
+echo.
+echo ----------------------------按"a"键 选择 构建后继续执行---------------------
+echo ----------------------------按"b"键 选择 构建后不执行-----------------------
+echo ----------------------------按"q"键 选择 退出----------------------------
+echo.
+SET /P choice=请选择操作项:
+IF /I '%Choice:~0,1%'=='a' GOTO a
+IF /I '%Choice:~0,1%'=='b' GOTO b
+IF /I '%Choice:~0,1%'=='q' exit
+
+SET outpath=.\build\
+
+:a
+echo 开始编译
+python hyperbole.py build -r
+
+echo 开始运行
+cd .\build\
+hysteria-windows-amd64.exe server -c server.yaml
+
+
+:b
+echo 开始编译
+python hyperbole.py build -r
---
+++ build.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+
+python hyperbole.py build -r
